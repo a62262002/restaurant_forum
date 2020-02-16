fca3a93ef0acc452d27bab42ddf49f41812aa38b
Emily Liu <a62262002@gmail.com>
2020-02-16 20:56:23 +0800

    Install bcryptjs, add userController and signup page, install body-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const db = require("./models");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
@@ -10,5 +11,7 @@ app.set("view engine", "handlebars");
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./routes")(app);
