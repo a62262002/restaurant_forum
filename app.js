6ce7a66bfbc030d16de046e6586f7f1af730b311
Emily Liu <a62262002@gmail.com>
2020-02-16 11:51:22 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const db = require("./models");
 const app = express();
 const port = 3000;
+
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
