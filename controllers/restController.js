9334fc493b074a572d896f5b31afe9bd4808cdab
Emily Liu <a62262002@gmail.com>
2020-02-22 23:40:52 +0800

    Add categories navbar on restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -3,16 +3,19 @@ const Restaurant = db.Restaurant;
 const Category = db.Category;
 const restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ nest: true, raw: true, include: Category }).then(
-      restaurants => {
+    Restaurant.findAll({ include: Category }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50)
+      }));
+      Category.findAll().then(categories => {
         return res.render("restaurants", {
-          restaurants: restaurants.map(r => ({
-            ...r,
-            description: r.description.substring(0, 50)
-          }))
+          restaurants: data,
+          categories: JSON.parse(JSON.stringify(categories))
+          // categoryId: JSON.parse(JSON.stringify(categoryId))
         });
-      }
-    );
+      });
+    });
   },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
