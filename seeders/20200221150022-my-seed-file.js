8324cd828b51e39590a61ecd79e58a2fc6435bc5
Emily Liu <a62262002@gmail.com>
2020-02-22 17:04:12 +0800

    Add category seed data

--- seeders/20200221150022-my-seed-file.js
+++ seeders/20200221150022-my-seed-file.js
@@ -34,6 +34,24 @@ module.exports = {
       ],
       {}
     );
+    queryInterface.bulkInsert(
+      "Categories",
+      [
+        "中式料理",
+        "日本料理",
+        "義大利料理",
+        "墨西哥料理",
+        "素食料裡",
+        "美式料理",
+        "複合式料理"
+      ].map((item, index) => ({
+        id: index + 1,
+        name: item,
+        createdAt: new Date(),
+        updateAt: new Date()
+      })),
+      {}
+    );
     return queryInterface.bulkInsert(
       "Restaurants",
       Array.from({ length: 50 }).map(d => ({
@@ -44,7 +62,8 @@ module.exports = {
         image: faker.image.imageUrl(),
         description: faker.lorem.text(),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        CategoryId: Math.floor(Math.random() * 5) + 1
       })),
       {}
     );
@@ -52,6 +71,7 @@ module.exports = {
 
   down: (queryInterface, Sequelize) => {
     queryInterface.bulkDelete("Users", null, {});
+    queryInterface.bulkDelete("Categories", null, {});
     return queryInterface.bulkDelete("Restaurants", null, {});
   }
 };
