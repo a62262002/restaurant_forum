946ed912b8b9980908cc3aecfe7a999a4d4552bd
Emily Liu <a62262002@gmail.com>
2020-02-23 13:07:01 +0800

    Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,21 +1,38 @@
 const db = require("../models");
 const Restaurant = db.Restaurant;
 const Category = db.Category;
+const pageLimit = 10;
 const restController = {
   getRestaurants: (req, res) => {
+    let offset = 0;
     let whereQuery = {};
     let categoryId = "";
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit;
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId);
       whereQuery["CategoryId"] = categoryId;
     }
-    Restaurant.findAll({
+
+    Restaurant.findAndCountAll({
       nest: true,
       raw: true,
       include: Category,
-      where: whereQuery
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
+    }).then(result => {
+      // data for pagination
+      let page = Number(req.query.page) || 1;
+      let pages = Math.ceil(result.count / pageLimit);
+      let totalPage = Array.from({ length: pages }).map(
+        (item, index) => index + 1
+      );
+      let prev = page - 1 < 1 ? 1 : page - 1;
+      let next = page + 1 > pages ? pages : page + 1;
+      // clean up restaurant data
+      const data = result.rows.map(r => ({
         ...r,
         description: r.description.substring(0, 50)
       }));
@@ -23,11 +40,16 @@ const restController = {
         return res.render("restaurants", {
           restaurants: data,
           categories: JSON.parse(JSON.stringify(categories)),
-          categoryId: JSON.parse(JSON.stringify(categoryId))
+          categoryId: JSON.parse(JSON.stringify(categoryId)),
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
         });
       });
     });
   },
+
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category
