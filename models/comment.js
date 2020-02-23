94d81dba5d146cbe3ce63c7f80d43793d142dfe7
Emily Liu <a62262002@gmail.com>
2020-02-23 13:33:41 +0800

    Add association among users, comment, restaurants

--- models/comment.js
+++ models/comment.js
@@ -1,12 +1,17 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define('Comment', {
-    test: DataTypes.STRING,
-    UserId: DataTypes.INTEGER,
-    RestaurantId: DataTypes.INTEGER
-  }, {});
+  const Comment = sequelize.define(
+    "Comment",
+    {
+      test: DataTypes.STRING,
+      UserId: DataTypes.INTEGER,
+      RestaurantId: DataTypes.INTEGER
+    },
+    {}
+  );
   Comment.associate = function(models) {
-    // associations can be defined here
+    Comment.belongsTo(models.Restaurant);
+    Comment.belongsTo(models.User);
   };
   return Comment;
 };
