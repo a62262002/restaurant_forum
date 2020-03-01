04176388410b00bf41a33f3dea295103ea067b28
Emily Liu <a62262002@gmail.com>
2020-03-01 15:35:11 +0800

    Add dashboard page

--- controllers/restController.js
+++ controllers/restController.js
@@ -78,6 +78,15 @@ const restController = {
         });
       });
     });
+  },
+  getDashboard: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category, { model: Comment, include: [User] }]
+    }).then(restaurant => {
+      return res.render("dashboard", {
+        restaurant: JSON.parse(JSON.stringify(restaurant))
+      });
+    });
   }
 };
 
