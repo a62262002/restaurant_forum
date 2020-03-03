a862ba8943e8ab20d8f159a28700f0615957e367
Emily Liu <a62262002@gmail.com>
2020-03-03 22:31:41 +0800

    Create followship model and add followers and following to users

--- models/user.js
+++ models/user.js
@@ -24,6 +24,17 @@ module.exports = (sequelize, DataTypes) => {
       as: "LikedRestaurants"
     });
   };
+  User.belongsToMany(User, {
+    through: models.Followship,
+    foreignKey: "followingId",
+    as: "Followers"
+  });
+
+  User.belongsToMany(User, {
+    through: models.Followship,
+    foreignKey: "followerId",
+    as: "Followings"
+  });
 
   return User;
 };
